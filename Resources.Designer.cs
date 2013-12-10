7980df3f8696efad14d6b92355bf04e6620deedb
Eric Reed <ebr@mediabrowser3.com>
2013-12-10 11:43:34 -0500

    Close button on extra panel

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -411,6 +411,16 @@ namespace Chocolate {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Collapse {
+            get {
+                object obj = ResourceManager.GetObject("Collapse", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Colors {
