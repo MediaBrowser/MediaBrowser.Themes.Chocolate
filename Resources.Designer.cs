c48b6d7489dee573a3cab623a7ffa26aef026e45
Eric Reed <ebr@mediabrowser3.com>
2013-12-16 16:27:18 -0500

    Add search button

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -631,6 +631,16 @@ namespace Chocolate {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IconSearch {
+            get {
+                object obj = ResourceManager.GetObject("IconSearch", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Images {
