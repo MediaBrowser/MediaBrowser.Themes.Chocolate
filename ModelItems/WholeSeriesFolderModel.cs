53d526c3c4eaef0719e0327155194bf2e91431f1
Eric Reed <ebr@mediabrowser3.com>
2014-01-27 20:57:29 -0500

    Fix recent list problem

--- ModelItems/WholeSeriesFolderModel.cs
+++ ModelItems/WholeSeriesFolderModel.cs
@@ -21,7 +21,7 @@ namespace Chocolate.ModelItems
             Async.Queue("Series Whole Child Load", () =>
             {
                 //trickle down to all our seasons - first load all children at base item level
-                foreach (var child in Folder.Children.OfType<Folder>())
+                foreach (var child in Folder.Children.OfType<Season>())
                 {
                     child.ReloadChildren();
                 }
